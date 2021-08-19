4b65c936af6f86f1ac1c6add89ec80daffc0ee28
narender-ceq <narender.singh@cloudeq.com>
2021-08-19 18:17:50 +0530

    add sitemap file

---
+++ resources/views/sitemap.blade.php
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+<!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
+
+
+<url>
+  <loc>http://securehealthgods.in/</loc>
+  <lastmod>2021-08-19T12:36:48+00:00</lastmod>
+  <priority>1.00</priority>
+</url>
+<url>
+  <loc>http://securehealthgods.in/home</loc>
+  <lastmod>2021-08-19T12:36:48+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>http://securehealthgods.in/product</loc>
+  <lastmod>2021-08-19T12:36:48+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>http://securehealthgods.in/gallery</loc>
+  <lastmod>2021-08-19T12:36:48+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>http://securehealthgods.in/youtube-videos</loc>
+  <lastmod>2021-08-19T12:36:48+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>http://securehealthgods.in/about-us</loc>
+  <lastmod>2021-08-19T12:36:48+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>http://securehealthgods.in/contact-us</loc>
+  <lastmod>2021-08-19T12:36:48+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>http://securehealthgods.in/login</loc>
+  <lastmod>2021-08-19T12:36:48+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>http://securehealthgods.in/buy-product</loc>
+  <lastmod>2021-08-19T12:36:48+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>http://securehealthgods.in/privacy-policy</loc>
+  <lastmod>2021-08-19T12:36:48+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>http://securehealthgods.in/password/reset</loc>
+  <lastmod>2021-08-19T12:36:48+00:00</lastmod>
+  <priority>0.64</priority>
+</url>
+
+
+</urlset>
--- routes/web.php
+++ routes/web.php
@@ -137,3 +137,8 @@ Route::post('/member-status/{id}','MemberController@memberIdTake');
 // payment
 Route::get('/member-payment/{id}','MemberPaymentController@payFee');
 Route::post('/member-payment-call-back', 'PaymentController@paytmCallback');
+
+Route::get('/sitemap',function(){
+    return response()->view('sitemap')
+      ->header('Content-Type', 'xml');
+});
