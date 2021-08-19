54992aa7e4a6ea856671f5cf3f45191b1cc917bf
narender-ceq <narender.singh@cloudeq.com>
2021-08-19 18:33:07 +0530

    search console add

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="@if(isset($description)) {{$description}} @endif">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <meta name="google-site-verification" content="GepccWm_M384hcZ_LsUELFhZO7saTPzAnrAUm7oQM0k" />
     <link rel="canonical" href="http://www.securehealthgods.in/" />
     <link rel="icon" href="{{config('app.file_path')}}/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" type="text/css" href="{{config('app.file_path')}}/bootstrap/css/bootstrap.min.css">
